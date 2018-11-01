d882759e55838254f18d0c905d524dfa7d072b9f
Vladislav Skripnichenko <Vladmoto2009@gmail.com>
2018-11-02 01:01:14 +0200

    Change email on username in auth

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'username' => 'admin',
+            //'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
